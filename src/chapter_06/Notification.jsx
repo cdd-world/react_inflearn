215385e9990b7388994777aeade224eeb228752b
cdd <cdd@cdds-MacBook-Pro.local>
2022-11-20 23:30:28 +0900

    chapter 8

--- src/chapter_06/Notification.jsx
+++ src/chapter_06/Notification.jsx
@@ -18,8 +18,18 @@ const styles = {
 class Notification extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {};
+    }
+
+    componentDidMount() {
+        console.log(`${this.props.id} componentDidMount()`);
+    }
+    componentDidUpdate() {
+        console.log(`${this.props.id} componentDidUpdate()`)
+    }
+    componentWillUnmount() {
+        console.log(`${this.props.id} componentWillUnmount()`)
     }
 
     render() {
