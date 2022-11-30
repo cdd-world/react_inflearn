1cd2e129eaa74aef02b97898befea068010dbaae
cdd <cdd@cdds-MacBook-Pro.local>
2022-11-30 21:10:55 +0900

    chapter 09

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
-import Accommodate from './chapter_07/Accommodate';
+// import Accommodate from './chapter_07/Accommodate';
 // import ConfirmButton from './chapter_08/ConfirmButton';
-// import LandingPage from './chapter_09/LandingPage';
+import LandingPage from './chapter_09/LandingPage';
 // import AttendanceBook from './chapter_10/AttendanceBook';
 // import SignUp from './chapter_11/SignUp';
 // import Calculator from './chapter_12/Calculator';
@@ -21,7 +21,7 @@ import Accommodate from './chapter_07/Accommodate';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <LandingPage />
   </React.StrictMode>
 );
 
---
+++ src/note/note.jsx
@@ -0,0 +1,8 @@
+function WarningBanner(props) {
+    if (!props.warning) {
+        return null;
+    }
+    return (
+        <div>경고 !!</div>
+    )
+}
